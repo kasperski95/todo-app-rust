73fc4d897ab74d77223fd22e5894b6d7f91e9111
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2023-01-07 23:18:27 +0100

    add and use clap package

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    name: String,
+    storage_path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    let content = std::fs::read_to_string(&args.storage_path).unwrap();
+    println!("Hello, {}!. File content: {}", args.name, content);
 }
