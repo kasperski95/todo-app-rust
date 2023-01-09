884d74ee147f0eb9307be9b383abb2170df96b5d
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2023-01-09 21:53:53 +0100

    setup basic cli structure

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,41 @@
-use anyhow::{Context, Result};
-use clap::Parser;
+use anyhow::Result;
+use clap::{Args, Parser, Subcommand};
 
+#[derive(Args)]
+struct AddArgs {
+    item: String,
+    #[arg(short, long)]
+    storage_path: std::path::PathBuf,
+}
+
+#[derive(Args)]
+struct LsArgs {
+    #[arg(short, long)]
+    storage_path: std::path::PathBuf,
+}
+
+#[derive(Subcommand)]
+enum Action {
+    Add(AddArgs),
+    Ls(LsArgs),
+}
 #[derive(Parser)]
 struct Cli {
-    name: String,
-    storage_path: std::path::PathBuf,
+    #[clap(subcommand)]
+    action: Action,
 }
 
 fn main() -> Result<()> {
     let args = Cli::parse();
-    let content = std::fs::read_to_string(&args.storage_path)
-        .with_context(|| format!("could not read file `...`"))?;
 
-    println!("Hello, {}!. File content: {}", args.name, content);
+    match args.action {
+        Action::Add(_) => {
+            println!("ADD")
+        }
+        Action::Ls(_) => {
+            println!("LS")
+        }
+    }
+
     Ok(())
 }
