01c7b823a67e6cecaa03134fdd5b3c587bc25707
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2023-01-15 17:55:12 +0100

    refactor test

---
+++ tests/app_util.rs
@@ -0,0 +1,34 @@
+use assert_cmd::{assert::Assert, prelude::*};
+use std::process::Command;
+use tempdir::TempDir;
+
+pub struct App {
+    storage_path: String,
+}
+
+impl App {
+    pub fn from_temp_file() -> App {
+        let tmp_dir = TempDir::new("_").unwrap();
+        let storage_path = String::from(tmp_dir.path().to_str().unwrap()) + "todo.json";
+        App { storage_path }
+    }
+
+    pub fn add<T: AsRef<str>>(&self, item: T) -> Assert {
+        Command::cargo_bin("todo")
+            .unwrap()
+            .arg("--storage-path")
+            .arg(&self.storage_path)
+            .arg("add")
+            .arg(item.as_ref())
+            .assert()
+    }
+
+    pub fn ls(&self) -> Assert {
+        Command::cargo_bin("todo")
+            .unwrap()
+            .arg("--storage-path")
+            .arg(&self.storage_path)
+            .arg("ls")
+            .assert()
+    }
+}
--- tests/e2e.rs
+++ tests/e2e.rs
@@ -1,26 +1,14 @@
-use assert_cmd::prelude::*;
-use std::process::Command;
-use tempdir::TempDir;
+use crate::app_util::App;
+
+mod app_util;
 
 #[test]
-fn add_item() -> Result<(), Box<dyn std::error::Error>> {
-    let tmp_dir = TempDir::new("_")?;
-    let storage_file_path = String::from(tmp_dir.path().to_str().unwrap()) + "todo.json";
+fn it_adds_and_lists_items() {
+    let app = App::from_temp_file();
 
-    let add_item_assertion = Command::cargo_bin("todo")?
-        .arg("--storage-path")
-        .arg(&storage_file_path)
-        .arg("add")
-        .arg("item 1")
-        .assert();
-
-    let list_items_assertion = Command::cargo_bin("todo")?
-        .arg("--storage-path")
-        .arg(&storage_file_path)
-        .arg("ls")
-        .assert();
+    let add_item_assertion = app.add("item 1");
+    let list_items_assertion = app.ls();
 
     add_item_assertion.stdout("OK\n");
     list_items_assertion.stdout("item 1\n");
-    Ok(())
 }
